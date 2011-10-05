0c4e91e1a43c410af140eef2d4bedb874599e819
Pierre Bellec <pierroweb@gmail.com>
2011-10-05 23:22:55 +0100

    added updateGoogleScore via ugly POST request using curl

--- Entity/Url.php
+++ Entity/Url.php
@@ -247,7 +247,7 @@ class Url
 		// we can retrieve multiple results, see http://graph.facebook.com/?ids=http://google.fr/,http://google.com
 		// could be cool to update lots of links at the same time
 	
-		$facebookUrlAsk = "http://graph.facebook.com/?ids=" . $this.getAddress();
+		$facebookUrlAsk = "http://graph.facebook.com/?ids=" . $this->getAddress();
 
 	    $jsonData = file_get_contents($facebookUrlAsk,0,null,null);
 	    $jsonAsArray = json_decode($jsonData,true);
@@ -269,7 +269,7 @@ class Url
 	public function twitterScoreCheck() 
 	{
 		// example : http://otter.topsy.com/stats.js?url=http://qpleple.com
-		$twitterUrlAsk = "http://otter.topsy.com/stats.js?url=" . $this.getAddress();
+		$twitterUrlAsk = "http://otter.topsy.com/stats.js?url=" . $this->getAddress();
 		
 		$jsonData = file_get_contents($twitterUrlAsk,0,null,null);
 		$jsonAsArray = json_decode($jsonData,true);
@@ -279,9 +279,32 @@ class Url
 			$this->updateTotalScore();
 		}
 	}
+	public function googleScoreCheck() 
+	{
+		// see for ex http://www.tomanthony.co.uk/blog/google_plus_one_button_seo_count_api/comment-page-1/ for the method
+		
+		$ch = curl_init();   
+	 	curl_setopt($ch, CURLOPT_URL, "https://clients6.google.com/rpc?key=AIzaSyCKSbrvQasunBoV16zDH9R33D88CeLr9gQ"); 
+	 	curl_setopt($ch, CURLOPT_POST, 1);
+	 	curl_setopt($ch, CURLOPT_POSTFIELDS, '[{"method":"pos.plusones.get","id":"p","params":{"nolog":true,"id":"' . $this->getAdress() . '","source":"widget","userId":"@viewer","groupId":"@self"},"jsonrpc":"2.0","key":"p","apiVersion":"v1"}]');
+	 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	 	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
 
 
+	 	$jsonData = curl_exec ($ch);
+	 	curl_close ($ch);
+	 	$jsonAsArray = json_decode($curl_results, true);
 
+	 	$lastCounts = (integer) $jsonAsArray[0]['result']['metadata']['globalCounts']['count'];
+
+		if(not($lastCounts == $this->getTwitterScore())) {
+			$this->setGoogleScore($lastCounts);
+			$this->updateTotalScore();
+		}
+	}
+
+
+i
 
 
 }
